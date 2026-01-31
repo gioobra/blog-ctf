f00cad0f30426fa10353b8a8dabecd3308a908d7
giobra <gioobrandino@gmail.com>
2026-01-31 14:20:35 -0300

    Initialize FastAPI application with database setup and model definitions

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from blog-ctf!")
+import fastapi
+import models
+from .database import SessionLocal
+from .database import engine
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-if __name__ == "__main__":
-    main()
+models.Base.metadata.create_all(bind=engine)
+app = fastapi()
